68210957b846158ab866fd14993e3f954d75de04
DeanBrighton <45199306+DeanBrighton@users.noreply.github.com>
2023-07-27 15:30:25 +1000

    Updated

--- src/havitblazordemo/havitblazordemo/Models/TenantViewModel.cs
+++ src/havitblazordemo/havitblazordemo/Models/TenantViewModel.cs
@@ -9,6 +9,7 @@ namespace EndpointDesignWeb.WebUI.Models
 
         [Required(ErrorMessage = "Tenant Display Name is required.")]
         [MaxLength(50, ErrorMessage = "Maximum length of text is 50 letters.")]
+        [MinLength(3, ErrorMessage = "The name needs to be at least 3 characters")]
         public string DisplayName { get; set; } = "";
 
         [MaxLength(50, ErrorMessage = "Maximum length of text is 50 letters.")]
